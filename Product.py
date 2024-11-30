b118994df6652d36d17bb1fd3da3d36f9baecbd5
алексей загородников <ayayv.ayayayv@yandex.ru>
2024-11-30 13:23:32 +0300

    Finished

--- Product.py
+++ Product.py
@@ -1,8 +1,34 @@
 class Product:
+    """
+    Класс, представляющий продукт с именем, весом и категорией.
+
+    Атрибуты:
+        name (str): Название продукта.
+        weight (float): Вес продукта в килограммах.
+        category (str): Категория, к которой относится продукт.
+
+    Методы:
+        __str__(): Возвращает строковое представление объекта `Product`.
+    """
+    
     def __init__(self, name, weight, category):
+        """
+        Инициализация нового продукта.
+
+        Аргументы:
+            name (str): Название продукта.
+            weight (float): Вес продукта в килограммах.
+            category (str): Категория продукта (например, "Еда", "Одежда").
+        """
         self.name = name
         self.weight = weight
         self.category = category
         
     def __str__(self):
+        """
+        Возвращает строковое представление объекта `Product`.
+
+        Возвращает:
+            str: Строковое представление продукта в формате "name, weight, category".
+        """
         return(f'{self.name}, {self.weight}, {self.category}')
--- Shop.py
+++ Shop.py
@@ -1,16 +1,45 @@
 class Shop:
-
     def __init__(self):
+        """
+        Инициализация менеджера продуктов, который использует файл для хранения данных о продуктах.
+
+        Атрибуты:
+            __file_name (str): Имя файла, в котором хранятся данные о продуктах (по умолчанию 'products.txt').
+        """
         self.__file_name = 'products.txt'
         
     def get_products(self):
+        """
+        Читает список продуктов из файла.
+
+        Возвращает:
+            str: Содержимое файла с данными о продуктах, разделённое символами новой строки.
+        """
         with open(self.__file_name) as file:
-            for string in file:
-                return string
+            return file.read()
 
-    def add(self,*products):
-        list_product = self.get_products()
+    def add(self, *products):
+        """
+        Добавляет новые продукты в файл, если их нет в списке.
 
-        if list_product is not [x for x in products]:
-            with open(self.__file_name, 'w') as file:
-                file.write(file)
+        Аргументы:
+            *products (Product): Продукты для добавления. Каждый продукт должен быть экземпляром класса `Product`.
+
+        Выводит:
+            str: Сообщение, если продукт уже есть в магазине.
+        """
+        list_product = self.get_products().split('\n')
+
+        # Создаем множество с именами уже существующих продуктов
+        existing_names = {line.split(', ')[0] for line in list_product}
+
+        # Открываем файл для записи новых продуктов
+        with open(self.__file_name, 'w') as file:
+            for product in products:
+                
+                # Проверка, если продукт уже существует
+                if product.name in existing_names:
+                    print(f'Продукт {product} уже есть в магазине')
+                else:
+                    # Записываем новый продукт в файл
+                    file.write(str(product) + '\n')
